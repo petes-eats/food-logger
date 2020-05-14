cc5d5f5dc690d653d7e8461261028dcfb19a5029
awu556 <austinw556@gmail.com>
2020-05-14 15:08:18 -0400

    singleRecipe work

--- src/components/singleRecipe.tsx
+++ src/components/singleRecipe.tsx
@@ -1,10 +1,33 @@
 import React from "react";
+import { Recipe } from "./allRecipes";
+import { mockRecipes } from "./allRecipes";
 
-export default function singleRecipe(): any {
-  return (
+const getSingleRecipe = (recipeId: number): Promise<Recipe> => {
+  return new Promise((resolve, reject) => {
+    try {
+      setTimeout(() => {
+        resolve(mockRecipes[recipeId - 1]);
+      }, 1000);
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
+
+export const SingleRecipe: React.FunctionComponent<any> = (props) => {
+  const emptyObj: any = {};
+  const [recipe, setRecipe] = React.useState(emptyObj);
+
+  React.useEffect(() => {
+    getSingleRecipe(props.match.params.recipeId)
+      .then((recipe) => setRecipe(recipe))
+      .catch((error) => console.error(error));
+  }, []);
+  console.log(props.match.params, "ALEX WHY");
+  return recipe ? (
     <div>
-      <h1>This is a single Recipe</h1>
+      <h1>{recipe.name}</h1>
       <h2>We will pull this with GraphQL...soon</h2>
     </div>
-  );
-}
+  ) : null;
+};
